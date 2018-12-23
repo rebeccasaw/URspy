8387976d7fdb96d1ea6ee5ab4d43bf9760c2e151
Rebecca Saw <rebeccasaw@rebeccas-air>
2018-12-23 23:10:43 +0000

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Rebecca Saw on 23/12/2018.
+//  Copyright © 2018 Rebecca Saw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
